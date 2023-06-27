0c91e4fb8aa692d6a0071e21ec414036a35aa6f1
Jose Ortiz <joseortizgomez90@gmail.com>
2023-06-27 19:35:59 +0200

    Esqueleto inicial del proyecto

---
+++ main.py
@@ -0,0 +1,11 @@
+from HLF.src.player import Player
+from HLF.src.tablero import Tablero 
+
+tablero_jugador = Tablero()
+tablero_maquina = Tablero()
+
+jugador = Player()
+maquina = Player()
+
+print(jugador)
+print(tablero_maquina)
